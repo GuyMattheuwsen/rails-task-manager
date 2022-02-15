5dcf62a7ef77e89ca1589e2ed5bf76249e7dc9fd
GuyMattheuwsen <guy.mattheuwsen@gmail.com>
2022-02-15 15:28:29 +0100

    index / list = ok

--- app/controllers/tasks_controller.rb
+++ app/controllers/tasks_controller.rb
@@ -2,4 +2,31 @@ class TasksController < ApplicationController
   def index
     @tasks = Task.all
   end
+
+  def show
+    @task = Task.find(params[:id])
+  end
+
+  def new
+    @task = Task.new # needed to instantiate the form_for
+  end
+
+  def create
+    @task = Task.new(task_params)
+    @task.save
+    # no need for app/views/tasks/create.html.erb
+    redirect_to task_path(@task)
+  end
+
+  def update
+    @task = Task.find(params[:id])
+    @task.update(params[:task])
+    # Will raise ActiveModel::ForbiddenAttributesError
+  end
+
+  private
+
+  def task_params
+    params.require(:task).permit(:title, :details)
+  end
 end
